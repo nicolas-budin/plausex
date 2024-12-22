b7c77c9cb3e34fcd6a61b264cea478fae46a0217
nicolas-budin <np.budin@gmail.com>
2024-12-22 10:48:08 +0100

    initial commit

---
+++ src/main/java/org/louhan/plausex/PlausexApplication.java
@@ -0,0 +1,13 @@
+package org.louhan.plausex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlausexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlausexApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/louhan/plausex/PlausexApplicationTests.java
@@ -0,0 +1,13 @@
+package org.louhan.plausex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlausexApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
